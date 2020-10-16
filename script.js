69b147f6d35cd938a48ecbf69eaacec4bb6d6bfc
tanoshiibot <olivia.fantinel@gmail.com>
2020-10-16 14:46:57 +0200

    shoot est maintenant une propriété de l'objet ship, cela permet beaucoup plus de flexibilité avec le code

--- script.js
+++ script.js
@@ -8,16 +8,20 @@ const ship = {
     "rot": 0,
     "velx": 0,
     "vely": 0,
-    "shoot1x": -30,
-    "shoot1y": -30,
-    "shoot2x": -30,
-    "shoot2y": -30,
-    "shoot3x": -30,
-    "shoot3y": -30,
-    "j": 0,
-    "k": 0,
-    "l": 0
+    "maxShoot": 3,
+    "currentShoot": [undefined, undefined, undefined],
+    "status": "alive"
 };
+
+class Shoot {
+    constructor(x, y, rot) {
+        this.x = x;
+        this.y = y;
+        this.rot = rot;
+        this.i = 0;
+        this.explosion = false;
+    }
+}
 
 class Asteroid {
     constructor(x, y, rad, rot, velx, vely) {
@@ -30,7 +34,6 @@ class Asteroid {
     }
 }
 
-const asteroid1 = new Asteroid(50, 50, 50, 2, 4);
 
 const pointer = [0, 0];
 
@@ -67,97 +70,95 @@ function trajectory(element){
     element.y = (element.y + element.vely + 600) % 600;
 }
 
-let explosion = false;
+function newShoot() {
+    ship.currentShoot.forEach((element, i) => {
+        if (ship.currentShoot[i] == undefined){
+            ship.currentShoot[i] = new Shoot (ship.x, ship.y, ship.rot);
+            let int = setInterval(function(){shoot(int, i)}, 16.6);
+            throw {};
+        }
+    })
+}
 
-function shoot(object, inc, shootx, shooty, tra, interval){
-    object[inc]++;
-    if (object[inc] < 100) {
-        object[shootx] = (object[shootx] + (5 * Math.cos(tra)) + 600) % 600;
-        object[shooty] = (object[shooty] + (5 * Math.sin(tra)) + 600) % 600; 
-        document.getElementById("test").innerHTML = object[inc];
-    } else if ((object[inc] >= 100) && (object[inc] <= 120)) {
-        object[shootx] = object[shootx];
-        object[shooty] = object[shooty];
-        explosion = true;
-        document.getElementById("test").innerHTML = object[inc];
+function shoot(interval, i) {
+    ship.currentShoot[i]["i"]++;
+    if (ship.currentShoot[i]["i"] < 100) {
+        ship.currentShoot[i]["x"] = (ship.currentShoot[i]["x"] + (5 * Math.cos(ship.currentShoot[i]["rot"])) + 600) % 600;
+        ship.currentShoot[i]["y"] = (ship.currentShoot[i]["y"] + (5 * Math.sin(ship.currentShoot[i]["rot"])) + 600) % 600; 
+        
+    } else if ((ship.currentShoot[i]["i"] >= 100) && (ship.currentShoot[i]["i"] <= 120)) {
+        ship.currentShoot[i]["x"] = ship.currentShoot[i]["x"];
+        ship.currentShoot[i]["y"] = ship.currentShoot[i]["y"];
+        ship.currentShoot[i]["explosion"] = true;
+        document.getElementById("test").innerHTML = ship.currentShoot[i]["i"];
 
-    } else if (object[inc] > 120) {
-        explosion = false;
-        object[inc] = 0;
-        object[shootx] = -30;
-        object[shooty] = -30;
+    } else if (ship.currentShoot[i]["i"] > 120) {
+        ship.currentShoot[i]["explosion"] = false;
+        ship.currentShoot[i]["i"] = 0;
+        ship.currentShoot[i] = undefined;
         clearInterval(interval);
     }
 }
 
-function explosionAnimation(c, x, y, i) {
-    c.moveTo(x, y);
-    if (explosion == false) {
-        c.arc(x, y, 5 + i / 100 , 0, 2 * Math.PI,);
-    } else {
-        c.moveTo(x, y)
-        c.lineTo(x + (5 + i / 100) * Math.cos(Math.PI / 4), y + (5 + i / 100) * Math.sin(Math.PI / 4));
-        c.moveTo(x, y);
-        c.lineTo(x + (5 + i / 100) * Math.cos(Math.PI / 2), y + (5 + i / 100) * Math.sin(Math.PI / 2));
-        c.moveTo(x, y);
-        c.lineTo(x + (5 + i / 100) * Math.cos(3 * Math.PI / 4), y + (5 + i / 100) * Math.sin(3 * Math.PI / 4));
-        c.moveTo(x, y);
-        c.lineTo(x + (5 + i / 100) * Math.cos(5 * Math.PI / 4), y + (5 + i / 100) * Math.sin(5 * Math.PI / 4));
-        c.moveTo(x, y);
-        c.lineTo(x + (5 + i / 100) * Math.cos(3 * Math.PI / 2), y + (5 + i / 100) * Math.sin(3 * Math.PI / 2));
-        c.moveTo(x, y);
-        c.lineTo(x + (5 + i / 100) * Math.cos(5 * Math.PI / 4), y + (5 + i / 100) * Math.sin(5 * Math.PI / 4));
-    
+
+function shootAnimation(shoot) {
+    if (shoot != undefined){
+        ctx.moveTo(shoot["x"], shoot["y"]);
+        if (shoot["explosion"] == false) {
+            ctx.arc(shoot["x"], shoot["y"], 5 + shoot["i"] / 50 , 0, 2 * Math.PI,);
+            ctx.fill();
+        } else {
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(Math.PI / 4), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(Math.PI / 4));
+            ctx.moveTo(shoot["x"], shoot["y"]);
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(Math.PI / 2), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(Math.PI / 2));
+            ctx.moveTo(shoot["x"], shoot["y"]);
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(3 * Math.PI / 4), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(3 * Math.PI / 4));
+            ctx.moveTo(shoot["x"], shoot["y"]);
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(5 * Math.PI / 4), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(5 * Math.PI / 4));
+            ctx.moveTo(shoot["x"], shoot["y"]);
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(3 * Math.PI / 2), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(3 * Math.PI / 2));
+            ctx.moveTo(shoot["x"], shoot["y"]);
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(7 * Math.PI / 4), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(5 * Math.PI / 4));
+            ctx.fill();
+        }
     }
 }
 
-function drawCanvas() {
-    document.getElementById("position").innerHTML = [~~ship.x, ~~ship.y, ship.rot, ~~ship.shootx, ~~ship.shooty];
+function drawShip() {
+    if (ship.status == "alive"){
+        ctx.moveTo((ship.x + (15 * Math.cos(ship.rot))), ship.y + (15 * Math.sin(ship.rot)));
+        ctx.lineTo(ship.x + (15 * Math.cos(ship.rot + (3 * Math.PI / 4))), ship.y + (15 * Math.sin(ship.rot + (3 * Math.PI / 4))));
+        ctx.lineTo(ship.x + (5 * Math.cos(ship.rot + Math.PI)), ship.y + (5 * Math.sin(ship.rot + Math.PI)));
+        ctx.lineTo(ship.x + (15 * Math.cos(ship.rot + (5 * Math.PI / 4))), ship.y + (15 * Math.sin(ship.rot + (5 * Math.PI / 4))));
+        ctx.lineTo(ship.x + (15 * Math.cos(ship.rot)), ship.y + (15 * Math.sin(ship.rot)));
+    }
+}
+
+function frame() {
+    document.getElementById("position").innerHTML = [~~ship.x, ~~ship.y, ship.rot];
     ctx.clearRect(0, 0, 600, 600)
     rotation();
     trajectory(ship);
     ctx.beginPath();
-    ctx.moveTo((ship.x + (15 * Math.cos(ship.rot))), ship.y + (15 * Math.sin(ship.rot)));
-    ctx.lineTo(ship.x + (15 * Math.cos(ship.rot + (3 * Math.PI / 4))), ship.y + (15 * Math.sin(ship.rot + (3 * Math.PI / 4))));
-    ctx.lineTo(ship.x + (5 * Math.cos(ship.rot + Math.PI)), ship.y + (5 * Math.sin(ship.rot + Math.PI)));
-    ctx.lineTo(ship.x + (15 * Math.cos(ship.rot + (5 * Math.PI / 4))), ship.y + (15 * Math.sin(ship.rot + (5 * Math.PI / 4))));
-    ctx.lineTo(ship.x + (15 * Math.cos(ship.rot)), ship.y + (15 * Math.sin(ship.rot)));
-    explosionAnimation(ctx, ship.shoot1x, ship.shoot1y, ship.j);
-    explosionAnimation(ctx, ship.shoot2x, ship.shoot2y, ship.k);
-    explosionAnimation(ctx, ship.shoot3x, ship.shoot3y, ship.l);
+    drawShip();
+    ship.currentShoot.forEach((element) => {
+        shootAnimation(element);
+    })
     ctx.fill();
     ctx.stroke();
+
 }
 
-
-setInterval(drawCanvas, 16.6);
+setInterval(frame, 16.6);
 
 let a;
 
 document.getElementById("canvas").addEventListener("mousedown", () => {
     a = setInterval(accelerateShip, 16.6);
-
-    shootRot = ship.rot;
-    
-    if (ship.shoot1x == -30) {
-        ship.shoot1x = ship.x;
-        ship.shoot1y = ship.y;
-        let shootRot1 = ship.rot;
-        let b = setInterval(function(){shoot(ship, "j", "shoot1x", "shoot1y", shootRot1, b)}, 16.6);
-    } else if (ship.shoot2x == -30) {
-        let shootRot2 = ship.rot;
-        ship.shoot2x = ship.x;
-        ship.shoot2y = ship.y;
-        let c = setInterval(function(){shoot(ship, "k", "shoot2x", "shoot2y", shootRot2, c)}, 16.6);
-    } else if (ship.shoot3x == -30){
-        let shootRot3 = ship.rot;
-        ship.shoot3x = ship.x;
-        ship.shoot3y = ship.y;
-        let d = setInterval(function(){shoot(ship, "l", "shoot3x", "shoot3y", shootRot3, d)}, 16.6);
-    }
+    newShoot();
 })
 document.getElementById("canvas").addEventListener("mouseup", () => {
-    clearInterval(a);
+    clearInterval(a); //sometimes, the ship keeps track of the pointer, even without clicking. It could be because an interval is not cleared. I should look into that.
     document.getElementById("test").innerHTML = "false";
 })
 document.getElementById("canvas").addEventListener("pointermove", () => {
@@ -165,4 +166,6 @@ document.getElementById("canvas").addEventListener("pointermove", () => {
 
 })
 
-document.getElementById("canvas")
+document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("button").innerHTML = ship.currentShoot[2]["y"]
+})
