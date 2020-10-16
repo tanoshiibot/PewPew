0d64cb179f954827ac40d2d4faf9b571e1119478
tanoshiibot <olivia.fantinel@gmail.com>
2020-10-16 15:53:00 +0200

    essai de powerups, bullet + et range +

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const ship = {
     "velx": 0,
     "vely": 0,
     "maxShoot": 3,
-    "currentShoot": [undefined, undefined, undefined],
-    "status": "alive"
+    "currentShoot": [undefined],
+    "status": "alive",
+    "range": 100,
 };
 
 class Shoot {
@@ -77,23 +78,24 @@ function newShoot() {
             ship.currentShoot[i] = new Shoot (ship.x, ship.y, ship.rot);
             let int = setInterval(function(){shoot(int, i)}, 16.6);
             throw {};
+            //throw is the only way I found to stop a forEach. It works, but it says in the concole that there's an error. I would like to find another way to stop the forEach.
         }
     })
 }
 
 function shoot(interval, i) {
     ship.currentShoot[i]["i"]++;
-    if (ship.currentShoot[i]["i"] < 100) {
+    if (ship.currentShoot[i]["i"] < ship.range) {
         ship.currentShoot[i]["x"] = (ship.currentShoot[i]["x"] + (5 * Math.cos(ship.currentShoot[i]["rot"])) + 600) % 600;
         ship.currentShoot[i]["y"] = (ship.currentShoot[i]["y"] + (5 * Math.sin(ship.currentShoot[i]["rot"])) + 600) % 600; 
         
-    } else if ((ship.currentShoot[i]["i"] >= 100) && (ship.currentShoot[i]["i"] <= 120)) {
+    } else if ((ship.currentShoot[i]["i"] >= ship.range) && (ship.currentShoot[i]["i"] <= ship.range + 20)) {
         ship.currentShoot[i]["x"] = ship.currentShoot[i]["x"];
         ship.currentShoot[i]["y"] = ship.currentShoot[i]["y"];
         ship.currentShoot[i]["explosion"] = true;
         document.getElementById("test").innerHTML = ship.currentShoot[i]["i"];
 
-    } else if (ship.currentShoot[i]["i"] > 120) {
+    } else if (ship.currentShoot[i]["i"] > (ship.range + 20)) {
         ship.currentShoot[i]["explosion"] = false;
         ship.currentShoot[i]["i"] = 0;
         ship.currentShoot[i] = undefined;
@@ -106,18 +108,18 @@ function shootAnimation(shoot) {
     if (shoot != undefined){
         ctx.moveTo(shoot["x"], shoot["y"]);
         if (shoot["explosion"] == false) {
-            ctx.arc(shoot["x"], shoot["y"], 5 + shoot["i"] / 50 , 0, 2 * Math.PI,);
+            ctx.arc(shoot["x"], shoot["y"], 5, 0, 2 * Math.PI,);
             ctx.fill();
         } else {
-            ctx.moveTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(0), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(0));
-            ctx.lineTo(shoot["x"] + (shoot["i"] - 100) * Math.cos(Math.PI / 4), shoot["y"] + (shoot["i"] - 100) * Math.sin(Math.PI / 4));
-            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(Math.PI / 2), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(Math.PI / 2));
-            ctx.lineTo(shoot["x"] + (shoot["i"] - 100) * Math.cos(3 * Math.PI / 4), shoot["y"] + (shoot["i"] - 100) * Math.sin(3 * Math.PI / 4));
-            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(Math.PI), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(Math.PI));
-            ctx.lineTo(shoot["x"] + (shoot["i"] - 100) * Math.cos(5 * Math.PI / 4), shoot["y"] + (shoot["i"] - 100) * Math.sin(5 * Math.PI / 4));
-            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(3 * Math.PI / 2), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(3 * Math.PI / 2));
-            ctx.lineTo(shoot["x"] + (shoot["i"] - 100) * Math.cos(7 * Math.PI / 4), shoot["y"] + (shoot["i"] - 100) * Math.sin(5 * Math.PI / 4));
-            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - 50) * Math.cos(0), shoot["y"] + (shoot["i"] / 2 - 50) * Math.sin(0));
+            ctx.moveTo(shoot["x"] + (shoot["i"] / 2 - ship.range / 2) * Math.cos(0), shoot["y"] + (shoot["i"] / 2 - ship.range / 2) * Math.sin(0));
+            ctx.lineTo(shoot["x"] + (shoot["i"] - ship.range) * Math.cos(Math.PI / 4), shoot["y"] + (shoot["i"] - ship.range) * Math.sin(Math.PI / 4));
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - ship.range / 2) * Math.cos(Math.PI / 2), shoot["y"] + (shoot["i"] / 2 - ship.range / 2) * Math.sin(Math.PI / 2));
+            ctx.lineTo(shoot["x"] + (shoot["i"] - ship.range) * Math.cos(3 * Math.PI / 4), shoot["y"] + (shoot["i"] - ship.range) * Math.sin(3 * Math.PI / 4));
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - ship.range / 2) * Math.cos(Math.PI), shoot["y"] + (shoot["i"] / 2 - ship.range / 2) * Math.sin(Math.PI));
+            ctx.lineTo(shoot["x"] + (shoot["i"] - ship.range) * Math.cos(5 * Math.PI / 4), shoot["y"] + (shoot["i"] - ship.range) * Math.sin(5 * Math.PI / 4));
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - ship.range / 2) * Math.cos(3 * Math.PI / 2), shoot["y"] + (shoot["i"] / 2 - ship.range / 2) * Math.sin(3 * Math.PI / 2));
+            ctx.lineTo(shoot["x"] + (shoot["i"] - ship.range) * Math.cos(7 * Math.PI / 4), shoot["y"] + (shoot["i"] - ship.range) * Math.sin(5 * Math.PI / 4));
+            ctx.lineTo(shoot["x"] + (shoot["i"] / 2 - ship.range / 2) * Math.cos(0), shoot["y"] + (shoot["i"] / 2 - ship.range / 2) * Math.sin(0));
             ctx.fill();
         }
     }
@@ -162,8 +164,7 @@ document.addEventListener("mouseup", () => {
     // => when : it happens when you click on the canvas, and release the click anywhere else.
     //if you do it multiple times, you can increase the speed at a funny rate.
     //removed the getElementById("canvas") from the addEventListener, it now works as intended. 
-    
-    document.getElementById("test").innerHTML = "false";    
+ 
 })
 document.getElementById("canvas").addEventListener("pointermove", () => {
     pointerPos(event);
@@ -171,4 +172,12 @@ document.getElementById("canvas").addEventListener("pointermove", () => {
 
 document.getElementById("button").addEventListener("click", () => {
     document.getElementById("button").innerHTML = ship.currentShoot[2]["y"]
+})
+
+document.getElementById("range").addEventListener("click", () => {
+    ship.range = ship.range + 10;
+})
+
+document.getElementById("bullet").addEventListener("click", () => {
+    ship.currentShoot.push(undefined);
 })
